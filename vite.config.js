33c512c71cc83783f178b392428728e28af677e6
Grimes <snake3773@mail.ru>
2023-08-19 00:24:50 +0300

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/Credit-card-form',
+	base: '/Credit-card-form/',
 	plugins: [react()],
 	server: {
 		port: 7000,
