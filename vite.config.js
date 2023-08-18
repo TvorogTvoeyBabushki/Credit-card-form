0a11b79ea351c679848b6b87ed59c262f73fc11d
Grimes <snake3773@mail.ru>
2023-08-18 23:39:50 +0300

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 7000,
-    open: true
-  }
+export default defineConfig(({ command }) => {
+	const config = {
+		plugins: [react()],
+		server: {
+			port: 7000,
+			open: true
+		},
+		base: '/'
+	}
+
+	if (command !== 'serve') {
+		config.base = '/react-vite-gh-pages/'
+	}
+
+	return config
 })
